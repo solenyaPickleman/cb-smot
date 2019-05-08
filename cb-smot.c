04b67e0d6c6a72dbd76de5812b33b000e78d5922
solenyaPickleman <bradyadice@gmail.com>
2019-05-07 22:44:29 -0400

    implement function for haversine distance

--- cb-smot.c
+++ cb-smot.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+
+#define PI 3.14159265
 
 /* This is the C implementation of the Clustering-based approach for Discovering Interesting
  * Places in Trajectories by Palma et al,
@@ -12,14 +15,28 @@
 /* define struct data point as described in data description docs. 
  * latitude - index 0. .longitude - index 1. altitude - index 3, date - index 5, time - index 6
  * */
-typedef struct {
+struct Point{
 	double latitude;
 	double longitude;
 	double altitude;	
 	char* date;
 	char* time;
-} Point;
+} ;
 
+/* find distance between two Points */ 
+double haversineDistance(struct Point* p1, struct Point* p2) {
+	/*distance between lat and lon */
+	double dlat = (p2->latitude - p1->latitude) * PI / 180.0;
+	double dlon = (p2->longitude - p1->longitude) * PI / 180.0;
+	/*to rads */
+	double lat1 = p1->latitude * PI / 180.0;
+	double lat2 = p2->latitude * PI / 180.0;
+
+	double a = pow(sin(dlat / 2), 2) + pow( sin(dlon / 2), 2) * cos(lat1) * cos(lat2);
+	double rad = 6371.0;
+	double c = 2 * asin(sqrt(a));
+	return rad * c;
+}
 
 int main() {
 	FILE* datafile;
@@ -29,10 +46,10 @@ int main() {
 		return 1;
 	}
 	char line[200];
-	Point* point;
+	struct Point* point;
 	printf("TESTING");
 	while (fgets(line, sizeof line, datafile)) {
-		point = (Point*)malloc(sizeof(Point));
+		point = (struct Point*)malloc(sizeof(struct Point));
 		int index = 0;
 		char *tok;
 		char *saveptr; 
@@ -63,5 +80,17 @@ int main() {
 
 	fclose(datafile);
 	printf("CB-SMOT\n");
+	struct Point* p1 = (struct Point*)malloc(sizeof(struct Point)); 
+	struct Point* p2 = (struct Point*)malloc(sizeof(struct Point));
+	p1->latitude = 51.5007;
+	p1->longitude = 0.1246;
+	p2->latitude = 40.6892;
+	p2->longitude = 74.0445;
+	
+	printf("\nDistance between points : %f km\n", haversineDistance(p1,p2));
+
+	/* now that you have read in the data .... read cb smot and find out wazzup */
+
+
 	return 0;
 }
