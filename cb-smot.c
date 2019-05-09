63bd06760fa70f352df759b8d4f12c8fb1a12089
solenyaPickleman <bradyadice@gmail.com>
2019-05-08 21:47:33 -0400

    fixed 'timedelta' method,and added a method for extracting human readable time from timedeltas

--- cb-smot.c
+++ cb-smot.c
@@ -38,6 +38,72 @@ double haversineDistance(struct Point* p1, struct Point* p2) {
 	return rad * c;
 }
 
+/* find absolute value of difference in time between two Points (only meant for same day comparison
+ * Different days should not be compared when looking at daily trajectories */
+int timedelta(char* p1, char* p2) {
+	
+	/*get hour, minute, second */
+	char* t1 = strdup(p1);
+	char* t2 = strdup(p2);
+	char *tok;
+	char *saveptr;
+	int index = 0;
+	int p1_hour;
+	int p1_minute;
+	int p1_second;
+	int p2_hour;
+	int p2_minute;
+	int p2_second;
+	/* read t1 */
+	for (tok = strtok(t1, ":"); tok && *tok; tok = strtok(NULL, ":")) {
+		switch(index) {
+			case 0: 
+				p1_hour = atoi(tok);
+				break;
+			case 1:
+			        p1_minute = atoi(tok);	
+				break;
+			case 2 :
+			        p1_second = atoi(tok);	
+				break ;
+		}
+		index++;
+	}
+	index = 0;
+	/* read t2 */
+        for (tok = strtok_r(t2, ":", &saveptr); tok && *tok; tok = strtok_r(NULL, ":", &saveptr)) {
+		switch(index) {
+			case 0: 
+				p2_hour = atoi(tok);
+				break;
+			case 1:
+			        p2_minute = atoi(tok);	
+				break;
+			case 2 :
+			        p2_second = atoi(tok);	
+				break ;
+		}
+		index++;
+	}
+	/*convert to integers */
+	int p1_time = 3600*p1_hour + 60*p1_minute + p1_second;
+	int p2_time = 3600*p2_hour + 60*p2_minute + p2_second;
+
+	int delta = p1_time - p2_time;
+	return delta;
+}
+
+/* take timedelta , return string in format int [] [hourdelta ,minutedelta ,seconddelta] */
+int* parseTimeDelta(int timedelta) {
+	static int delta[3]; 
+	delta[0] = timedelta / 3600;
+	delta[1] = (timedelta%3600)/60;
+	delta[2] = (timedelta%3600)%60;
+	
+	return delta;
+}
+
+
 int main() {
 	FILE* datafile;
 	datafile = fopen("/home/sir/projects/cb-smot/test.plt", "r");
@@ -86,9 +152,18 @@ int main() {
 	p1->longitude = 0.1246;
 	p2->latitude = 40.6892;
 	p2->longitude = 74.0445;
-	
+	p1->time = malloc(strlen("23:34:40") + 1 );
+	p2->time = malloc(strlen("23:34:40") + 1 );
+	p1->time = "23:34:40";
+	p2->time = "21:40:20";	
+	printf("%s, %s", p1->time, p2->time);
 	printf("\nDistance between points : %f km\n", haversineDistance(p1,p2));
 
+	printf("%s --- %s" , p1->time, p2->time);
+	int td = timedelta(p2->time, p1->time);
+	int* parseddelta = parseTimeDelta(td);
+	printf("\nTime delta : %d seconds", td);
+	printf("\nTime delta : %d hours, %d minutes, %d seconds", parseddelta[0], parseddelta[1], parseddelta[2]);
 	/* now that you have read in the data .... read cb smot and find out wazzup */
 
 
